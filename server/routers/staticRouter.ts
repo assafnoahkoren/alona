ec28cd33a7c42d5a1b331a39aa113084fc49a414
Assaf Koren <assafnoahkoren@gmail.com>
2024-10-21 01:12:01 +0300

    .

--- server/routers/staticRouter.ts
+++ server/routers/staticRouter.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+ import { dirname } from 'path';
 
+ const __filename = fileURLToPath(import.meta.url);
+ const __dirname = dirname(__filename);
 const staticRouter = Router();
 
 // Serve static files from the 'static' directory
 staticRouter.use('/', (req, res) => {
-  const filePath = path.join(__dirname, 'static', req.path);
+  const filePath = path.join(__dirname, '../static', req.path);
   res.sendFile(filePath);
 });
 
