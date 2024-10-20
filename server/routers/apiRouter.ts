7e30318c196f685f862a20081fca0873d34faad7
Assaf Koren <assafnoahkoren@gmail.com>
2024-10-21 01:33:38 +0300

    .

--- server/routers/apiRouter.ts
+++ server/routers/apiRouter.ts
@@ -1,8 +1,35 @@
 import { Router } from 'express';
 import authenticatedRouter from './authenticatedRouter';
 import unauthenticatedRouter from './unauthenticatedRouter';
+import prisma from '../db';
 
 const apiRouter = Router();
+apiRouter.get('/', async (req, res) => {
+  const hotelsCount = await prisma.iDF_hotels.count();
+  const settlementsCount = await prisma.iDF_Settlement.count();
+  const roomsCount = await prisma.iDF_Rooms.count();
+  const bookingsCount = await prisma.iDF_Booking.count();
+
+  const runs = await prisma.algorithm_Run.count();
+  const allocations = await prisma.allocations.count();
+  const rooms_SnapShot = await prisma.rooms_SnapShot.count();
+
+  res.json({
+    views: {
+      hotels: hotelsCount,
+      settlements: settlementsCount,
+      rooms: roomsCount,
+      bookings: bookingsCount,
+    },
+    tables: {
+      runs,
+      allocations,
+      rooms_SnapShot,
+    },
+  });
+});
+
+
 
 apiRouter.use('/auth', authenticatedRouter);
 apiRouter.use('/public', unauthenticatedRouter);
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import prisma from './db';
 import staticRouter from './routers/staticRouter';
 import apiRouter from './routers/apiRouter';
 
@@ -7,30 +6,7 @@ const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
 // Define a simple route
-app.get('/', async (req: Request, res: Response) => {
-  const hotelsCount = await prisma.iDF_hotels.count();
-  const settlementsCount = await prisma.iDF_Settlement.count();
-  const roomsCount = await prisma.iDF_Rooms.count();
-  const bookingsCount = await prisma.iDF_Booking.count();
 
-  const runs = await prisma.algorithm_Run.count();
-  const allocations = await prisma.allocations.count();
-  const rooms_SnapShot = await prisma.rooms_SnapShot.count();
-
-  res.json({
-    views: {
-      hotels: hotelsCount,
-      settlements: settlementsCount,
-      rooms: roomsCount,
-      bookings: bookingsCount,
-    },
-    tables: {
-      runs,
-      allocations,
-      rooms_SnapShot,
-    },
-  });
-});
 
 app.use('/', staticRouter);
 app.use('/api', apiRouter);
