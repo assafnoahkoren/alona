d595fa1508ec4c856460b588a3b22fc900c8963a
Assaf Koren <assafnoahkoren@gmail.com>
2024-10-28 01:37:45 +0200

    .

---
+++ client/src/components/ActionCard.tsx
@@ -0,0 +1,23 @@
+import { Card, Group, Image, Text } from '@mantine/core';
+import { IconDots } from '@tabler/icons-react';
+import React from 'react';
+
+interface ActionCardProps {
+  title: string;
+  subtitle: string;
+}
+
+const ActionCard: React.FC<ActionCardProps> = ({ title, subtitle }) => {
+  return (
+    <Card shadow="sm" w={350} h={180} radius="md" className='flex justify-center cursor-pointer hover:shadow-md transition-shadow hover:scale-[1.01] scale-100 transition-transform'>
+      <Card.Section p="xl">
+        <IconDots size={20} opacity={0.5} className='absolute top-2 left-2'/>  
+        <Image src={`/money-illustration.png`} alt={title} w={52} mb={20} />
+        <Text fw={500}>{title}</Text>
+        <Text>{subtitle}</Text>
+      </Card.Section>
+    </Card>
+  );
+};
+
+export default ActionCard;
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,5 +1,18 @@
+import { Group, Stack, Title } from "@mantine/core";
+import ActionCard from "./ActionCard";
+
 const Home = () => {
-  return <div>Home</div>;
+  return <>
+    <Stack>
+      <Title order={3} className='p-4'>מה ברצונך לעשות?</Title>
+      <Group wrap="wrap">
+        <ActionCard title="צפייה במלונות" subtitle="צפייה במלונות והוספת מלון חדש" />
+        <ActionCard title="צפייה במלונות" subtitle="צפייה במלונות והוספת מלון חדש" />
+        <ActionCard title="צפייה במלונות" subtitle="צפייה במלונות והוספת מלון חדש" />
+        <ActionCard title="צפייה במלונות" subtitle="צפייה במלונות והוספת מלון חדש" />
+      </Group>
+    </Stack>
+  </>;
 };
 
 export default Home;
