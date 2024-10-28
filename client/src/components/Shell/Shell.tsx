c2988129e37da18b1b3ecfdac5bcc029c23cb4d4
Assaf Koren <assafnoahkoren@gmail.com>
2024-10-28 02:22:50 +0200

    .

--- client/src/components/Shell/Shell.tsx
+++ client/src/components/Shell/Shell.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { AppShell, Burger, Title, useMantineTheme } from '@mantine/core';
+import { AppShell, Divider, Group, Stack, Text, Title, useMantineTheme } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
-import { useDisclosure } from '@mantine/hooks';
 import { Squash as Hamburger } from 'hamburger-react'
+import { IconArchive, IconCar, IconDeviceFloppy, IconFilePlus, IconHome, IconLuggage, IconPlayerPlay } from '@tabler/icons-react';
 
 export function Shell() {
   const theme = useMantineTheme();
@@ -11,7 +11,7 @@ export function Shell() {
   return (
     <AppShell
       className='bg-[#F5F5F5]'
-      withBorder={false} 
+      withBorder={false}
       header={{ height: 60 }}
       navbar={{
         width: 200,
@@ -24,11 +24,22 @@ export function Shell() {
         <Title order={5}>מערכת שיבוץ מלונות</Title>
       </AppShell.Header>
 
-      <AppShell.Navbar p="md" >
-        <div className='flex items-start justify-end'>
+      <AppShell.Navbar p={0} >
+        <div className='flex items-start justify-end p-2'>
           <Hamburger toggled={opened} toggle={setOpen} size={48} />
-
         </div>
+        <Stack gap={0}>
+          <NavItem label='מסך בית' icon={<IconHome />} />
+          <NavItem isActive label='מעקב פינוי בפועל' icon={<IconPlayerPlay />} />
+          <NavItem label='יצירת תוכנית חדשה' icon={<IconFilePlus />} />
+          <NavItem label='טיוטות פרסום' icon={<IconDeviceFloppy />} />
+          <NavItem label='היסטוריה' icon={<IconArchive />} />
+          <Divider className='my-6' />
+
+          <Title order={6} className='px-5'>מסכי שליטה</Title>
+          <NavItem label='הערכת חדרי מלון נדרשים לפינוי ' icon={<IconCar />} />
+          <NavItem label='בתי מלון ' icon={<IconLuggage />} />
+        </Stack>
       </AppShell.Navbar>
 
       <AppShell.Main>
@@ -36,4 +47,28 @@ export function Shell() {
       </AppShell.Main>
     </AppShell>
   );
+}
+
+type NavItemProps = {
+  label: string,
+  icon: React.ReactNode,
+  isActive?: boolean
+}
+const NavItem = (props: NavItemProps) => {
+  let itemClassName = 'color-[#687E97] p-[6px] cursor-pointer hover:bg-blue-100 hover:bg-opacity-50';
+  let iconClassName = 'color-[#C3CFE7]';
+  if (props.isActive) {
+    itemClassName = `${itemClassName} bg-blue-100 bg-opacity-50 text-white border-[2px] border-e-solid border-blue-500`;
+    iconClassName = `${iconClassName} color-blue-600`;
+  }
+
+  return <Group className={itemClassName} gap={8}>
+    <div className={`flex-[1] flex items-center justify-end ${iconClassName}`}>
+      {props.icon}
+    </div>
+
+    <Text className="flex-[5] flex items-center justify-start overflow-hidden text-ellipsis whitespace-nowrap">
+      {props.label}
+    </Text>
+  </Group>
 }
