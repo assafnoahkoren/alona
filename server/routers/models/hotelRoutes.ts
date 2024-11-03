f3c26bff462f521d75ecde2dc84bcd22482c9ec4
Assaf Koren <assafnoahkoren@gmail.com>
2024-11-03 02:34:08 +0200

    .

--- server/routers/models/hotelRoutes.ts
+++ server/routers/models/hotelRoutes.ts
@@ -2,47 +2,6 @@ import express, { Request, Response } from "express";
 import prisma from "../../db";
 import { IDF_hotels, IDF_Rooms } from "@prisma/client";
 
-/**
- * @swagger
- * /api/hotels:
- *   get:
- *     summary: Retrieve all hotels with their rooms
- *     tags: [Hotels]
- *     responses:
- *       200:
- *         description: List of hotels with their rooms
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   Hotel_ID:
- *                     type: string
- *                   hotel_name:
- *                     type: string
- *                   City:
- *                     type: string
- *                   ADDRESS:
- *                     type: string
- *                   rooms:
- *                     type: array
- *                     items:
- *                       type: object
- *                       properties:
- *                         Room_ID:
- *                           type: integer
- *                         Hotel_ID:
- *                           type: string
- *                         Room_type:
- *                           type: string
- *                         free_room_count:
- *                           type: integer
- *       500:
- *         description: Server error
- */
-
 const router = express.Router();
 
 
