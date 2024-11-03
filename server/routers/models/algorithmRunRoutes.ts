92e0851ea62840c6f049684b09d0bf622145efe7
Assaf Koren <assafnoahkoren@gmail.com>
2024-11-03 08:23:25 +0200

    .

--- server/routers/models/algorithmRunRoutes.ts
+++ server/routers/models/algorithmRunRoutes.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import prisma from "../../db";
 import { Algorithm_Run } from "@prisma/client";
+import { EnrichedHotel } from "./hotelRoutes";
+import { EnrichedSettlement } from "./settlementRoutes";
+import algorithmService from "../../services/algorithmService";
 
 const router = express.Router();
 
@@ -25,12 +28,27 @@ router.get('/', async (_req: Request, res: Response<Algorithm_Run[]>) => {
   }
 });
 
+
+type Parameters = {
+  hotels?: EnrichedHotel[];
+  settlement?: EnrichedSettlement[];
+}
+
 router.post('/', async (req: Request<{}, {}, CreateAlgorithmRunDto>, res: Response) => {
   try {
     const newRun = await prisma.algorithm_Run.create({
-      data: req.body
+      data: {}
     });
-    res.status(201).json(newRun);
+    const parameters: Parameters = JSON.parse(req.body.parameters || '{}');
+    const result = await algorithmService.runAllocation({
+      hotels: parameters.hotels || [],
+      settlements: parameters.settlement || [],
+      personsInRooms: 4,
+    });
+    res.status(201).json({
+      algorithmRun: newRun,
+      result: result,
+    });
   } catch (error) {
     res.status(500).json({ error: 'Failed to create algorithm run' });
   }
--- server/services/algorithmService.ts
+++ server/services/algorithmService.ts
@@ -1,23 +1,65 @@
+import { IDF_Rooms } from "@prisma/client";
 import { EnrichedHotel } from "../routers/models/hotelRoutes";
 import { EnrichedSettlement } from "../routers/models/settlementRoutes";
 
 export interface AlgorithmInput {
-    hotels: EnrichedHotel[];
-    settlements: EnrichedSettlement[];
-    personsInRooms: number;
+  hotels: EnrichedHotel[];
+  settlements: EnrichedSettlement[];
+  personsInRooms: number;
 }
 
+type HotelID_RoomType_Key = `${string}-${string}`;
+type SettlementID = string;
+type RoomsByHotel = Record<HotelID_RoomType_Key, IDF_Rooms[]>;
 export interface AlgorithmOutput {
-    allocations: Record<string, Record<string, number>>;
-    unallocated: Record<string, number>;
-    duration: number;
+  allocations: Record<SettlementID, RoomsByHotel>;
+  unallocated: RoomsByHotel;
+  duration: number;
 }
 
 const algorithmService = {
-    runAllocation: async (input: AlgorithmInput): Promise<AlgorithmOutput> => {
-        // Implementation to be filled in later
-        throw new Error("Not implemented");
+  runAllocation: async (input: AlgorithmInput): Promise<AlgorithmOutput> => {
+    const availableRooms = getRooms(input.hotels);
+    const availableRoomsByHotel = groupRoomsByHotel(availableRooms);
+    const neededRooms = input.settlements.map((settlement) => ({
+      settlementID: settlement.Settlement_id,
+      rooms: settlement.Settlements_To_Evacuate[0].rooms_needed,
+    }));
+    return {
+      allocations: {},
+      unallocated: availableRoomsByHotel,
+      duration: 0,
+    };
+  },
+};
+
+const getRooms = (hotels: EnrichedHotel[]) => {
+  const rooms = hotels.flatMap((hotel) => hotel.rooms);
+  const groupedRooms: Record<string, IDF_Rooms> = {};
+
+  rooms.forEach((room) => {
+    if (!room.Room_type) return;
+    const key = `${room.Hotel_ID}-${room.Room_type}`;
+    if (!groupedRooms[key]) {
+      groupedRooms[key] = room;
+    } else {
+      groupedRooms[key].free_room_count += room.free_room_count;
     }
+  });
+
+  return groupedRooms;
+};
+
+const groupRoomsByHotel = (rooms: Record<string, IDF_Rooms>) => {
+  const roomsByHotel: RoomsByHotel = {};
+  for (const room of Object.values(rooms)) {
+    const key: HotelID_RoomType_Key = `${room.Hotel_ID}-${room.Room_type}`;
+    if (!roomsByHotel[key]) {
+      roomsByHotel[key] = [];
+    }
+    roomsByHotel[key].push(room);
+  }
+  return roomsByHotel;
 };
 
 export default algorithmService;
