afb4cc234745c169f8c703085bcda5d7168917bf
sagie.ivan <sagie012@gmail.com>
2024-11-05 00:23:32 +0200

    Add new settlement and remove

--- client/src/components/NewPlan.tsx
+++ client/src/components/NewPlan.tsx
@@ -1,11 +1,44 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { staticDataStore } from '../stores/static-data-store';
 import { observer } from 'mobx-react-lite';
-import { Button, Card, Group, Stack, Text, Title, Tooltip } from '@mantine/core';
-import { IconHome, IconBuilding, IconPlus, IconAccessible, IconUser, IconUsersGroup, IconWheelchair, IconDisabled, IconRun, IconCpu, IconBrain, IconReload } from '@tabler/icons-react';
+import {
+  ActionIcon,
+  Button,
+  Card,
+  Group,
+  LoadingOverlay,
+  Modal, NumberInput, Select,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+  Tooltip
+} from '@mantine/core';
+import {
+  IconHome,
+  IconBuilding,
+  IconPlus,
+  IconAccessible,
+  IconUser,
+  IconUsersGroup,
+  IconWheelchair,
+  IconDisabled,
+  IconRun,
+  IconCpu,
+  IconBrain,
+  IconReload,
+  IconSquareRoundedX, IconX
+} from '@tabler/icons-react';
 import { evacPlanStore } from '../stores/evac-plan-store';
+import { EnrichedSettlement } from '../../../server/routers/models/settlementRoutes.ts';
+import { modals } from '@mantine/modals';
+import settlementService from '../services/settlementService.ts';
+import { useDisclosure } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
 
 const NewPlan = () => {
+  const [createSettlementModalOpened, { open: openCreateSettlementModal, close: closeCreateSettlementModal}] = useDisclosure(false);
+
   useEffect(() => {
     staticDataStore.fetchData();
   }, []);
@@ -18,22 +51,46 @@ const NewPlan = () => {
     return <div>Error: {staticDataStore.error}</div>;
   }
 
+  const createSettlement = () => {
+  }
+
+  const removeSettlement = (settlementId: EnrichedSettlement['Settlement_id']) => {
+    modals.openConfirmModal({
+      title: 'הסרת יישוב',
+      children: (
+          <Text size="sm">
+            האם את/ה בטוח/ה שברוצנך להסיר את היישוב?
+          </Text>
+      ),
+      labels: { confirm: 'הסרה', cancel: 'ביטול' },
+      onConfirm: () => settlementService.remove(settlementId),
+    });
+  }
+
   return (
     <Stack justify='space-between' w='100%'>
       <Stack gap="xl" p='md'>
         <Stack>
           <Group>
             <Title order={3}>ישובים לפינוי</Title>
-            <Button variant="subtle" size='sm' leftSection={<IconPlus size={16} />}>הוסף</Button>
+            <Button variant="subtle" size='sm' leftSection={<IconPlus size={16} />} onClick={() => openCreateSettlementModal()}>הוסף</Button>
+            <Modal opened={createSettlementModalOpened} onClose={closeCreateSettlementModal} title="הוספת יישוב">
+              <SelectSettlementToEvacuate onClose={() => closeCreateSettlementModal()} />
+            </Modal>
           </Group>
           <Group>
             {staticDataStore.settlementsToEvacuate.map((settlement) => (
               <Card key={settlement.Settlement_id} shadow="sm" padding="lg" radius="md" withBorder w={280}>
-                <Group wrap='nowrap'>
-                  <IconHome size={24} color="#4C6EF5" />
-                  <Tooltip label={settlement.Name} multiline>
-                    <Text fw={500} className="truncate max-w-[120px]">{settlement.Name}</Text>
-                  </Tooltip>
+                <Group justify="space-between">
+                  <Group wrap='nowrap'>
+                    <IconHome size={24} color="#4C6EF5" />
+                    <Tooltip label={settlement.Name} multiline>
+                      <Text fw={500} className="truncate max-w-[120px]">{settlement.Name}</Text>
+                    </Tooltip>
+                  </Group>
+                  <ActionIcon variant="outline" color="red" radius="xl" aria-label="delete" size="xs" onClick={() => removeSettlement(settlement.Settlement_id)}>
+                    <IconX />
+                  </ActionIcon>
                 </Group>
                 <Text size="sm" c="dimmed">
                   מספר חדרים נדרשים: {settlement.Settlements_To_Evacuate[0]?.rooms_needed || 'לא ידוע'}
@@ -44,10 +101,7 @@ const NewPlan = () => {
         </Stack>
 
         <Stack>
-          <Group>
-            <Title order={3}>יעדי פינוי</Title>
-            <Button variant="subtle" size='sm' leftSection={<IconPlus size={16} />}>הוסף</Button>
-          </Group>
+          <Title order={3}>יעדי פינוי</Title>
           <Group wrap='wrap'>
             {staticDataStore.hotelsWithRooms.map((hotel) => (
               <Card key={hotel.Hotel_ID} shadow="sm" padding="lg" radius="md" withBorder w={280}>
@@ -91,5 +145,54 @@ function getRoomTypeIcon(roomType: string | null | undefined) {
   return roomTypeIcons.get(roomType) || null;
 }
 
+const SelectSettlementToEvacuate = ({onClose}: {onClose: () => void}) => {
+  const [loading, setLoading] = useState(false);
+  const form = useForm<{
+    settlement: EnrichedSettlement['Settlement_id'] | null,
+    numberOfRooms: number | null,
+  }>({
+    initialValues: {
+      settlement: null,
+      numberOfRooms: null,
+    },
+
+    validate: {
+      settlement: (value) => !value ? 'יש לבחור יישוב' : null,
+      numberOfRooms: (value) => !value ? 'יש לבחור מספר חדרים' : value <= 0 ? 'יש לבחור מספר חיובי' : null,
+    },
+  });
+
+  const handleSubmit = (values) => {
+    setLoading(true);
+    settlementService.create(values.settlement, values.numberOfRooms).then(() => {
+      onClose();
+    }).finally(() => {
+      setLoading(false);
+    });
+  };
+
+  return <form onSubmit={form.onSubmit(handleSubmit)}>
+    <LoadingOverlay visible={loading}/>
+    <Select
+        label="יישוב"
+        placeholder="בחירת יישוב"
+        required
+        data={staticDataStore.settlementsNotToEvacuate.map((settlement) => ({value: settlement.Settlement_id, label: settlement.Name}))}
+        {...form.getInputProps('settlement')}
+    />
+
+    <NumberInput
+        mt="md"
+        required
+        placeholder="מספר חדרים"
+        label="מספר חדרים"
+        {...form.getInputProps('numberOfRooms')}
+    />
+
+    <Button type="submit" mt="sm">
+      הוספה
+    </Button>
+  </form>;
+}
 
 export default observer(NewPlan);
