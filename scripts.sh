b724f5cd4a5ec1990a941d45116f7676d81292a5
Assaf Koren <assafnoahkoren@gmail.com>
2024-11-20 22:12:36 +0200

    .

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,3 @@
 docker run --env-file server/.env -p 8888:8888 alona-server:latest
 
-docker build -t alona-server:latest -t alona-server:0.1.4 . && docker save -o alona-server-0.1.4.tar alona-server:latest
+docker build -t alona-server:latest -t alona-server:0.1.5 . && docker save -o alona-server-0.1.5.tar alona-server:latest
